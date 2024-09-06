b8e5a397dac669311092b6b526a92cd5aac5efe6
ninaw04 <ninawangcn@gmail.com>
2024-09-06 15:11:45 -0700

    Initial Commit

---
+++ testProj/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testProj
+//
+//  Created by Nina Wang on 9/6/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
