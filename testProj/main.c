679d59163763dbb104a3a68136f8040db9510aa6
ninaw04 <ninawangcn@gmail.com>
2024-09-25 10:34:26 -0700

    :construction: committing gradient code, problem should be not using -1 array

--- testProj/main.c
+++ testProj/main.c
@@ -3,6 +3,7 @@
 //  CS136
 //
 //  Created by nha2 on 8/27/24.
+//  Edited by Nina Wang on 9/25/24
 // Test and demo program for netpbm. Reads a sample image and creates several output images.
 
 #include <stdio.h>
@@ -17,67 +18,50 @@ Image imageBlackWhite(Image originalImg, double threshold);
 Image imageExpand(Image originalImg);
 Image imageShrink(Image originalImg);
 Image imageNoise(Image originalImg, float percentage);
+Matrix componentLabeling(Image cleanImage);
+Image componentColoring(Image originalImg, Matrix components, int threshold);
 
 int main(int argc, const char * argv[]) {
     //-------------------------------------------------------------------------------
        //create blackWhiteImage:
-        // You need to change the path file: /Users/nha2/Downloads/Proj1_part1_2/Proj1_part1_2/ to your local directory
     Image inputImage = readImage("/Users/ninawang/Documents/School/CS136/testProj/netpbm/gray.pgm");
     Image cleanImage = readImage("/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
-    
-//    printf("what does rand do anyways: %d\n", rand());
-//    for (int i = 0; i < 10; i++) {
-//        printf("%f\n", (double)rand()/RAND_MAX);
-//    }
-    
-    Image noiseImage = imageNoise(cleanImage, 0.6);
-    writeImage(noiseImage, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/noise.pbm");
-
-       //-------------------------------------------------------------------------------
-       // COMMENT THIS FUNCTION IF YOU DON'T WANT IT TO RUN EVEY TIME
-//    Image rotatedImage = createImage(inputImage.height, inputImage.width);
-//    Image invertedImage = createImage(inputImage.height, inputImage.width);
-//       for (int x = 0; x < inputImage.width; x++)
-//           for (int y = 0; y < inputImage.height; y++)
-//           {
-//               rotatedImage.map[y][x] = inputImage.map[inputImage.height - y - 1][inputImage.width - x - 1];
-//
-//               invertedImage.map[y][x].r = 255 - inputImage.map[y][x].r;
-//               invertedImage.map[y][x].g = 255 - inputImage.map[y][x].g;
-//               invertedImage.map[y][x].b = 255 - inputImage.map[y][x].b;
-//               // Let's just ignore 'i' here; it's irrelevant if we want to save image as PPM.
-//           }
-//       writeImage(rotatedImage, "/Users/ninawang/Documents/School/CS136/project1/netpbm/rotated.pbm");
-//       writeImage(invertedImage, "/Users/ninawang/Documents/School/CS136/project1/netpbm/inverted.pbm");
-//       writeImage(inputImage, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/gray.pgm");
-//       writeImage(inputImage, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/black-white.pbm");
+//    Image test = readImage("/Users/ninawang/Documents/School/CS136/testProj/netpbm/shenanigans.pbm");
 
     //-------------------------------------------------------------------------------
+        // create colored text image:
+    printf("finding components\n");
+    Matrix components = componentLabeling(cleanImage);
+    printf("coloring components\n");
+    Image textColoredImage = componentColoring(cleanImage, components, 200);
+    printf("done with components\n");
+    writeImage(textColoredImage, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/text_colored.ppm");
+    
         //  create blackWhiteImage:
-    printf("creating black white image\n");
-    Image blackWhite = imageBlackWhite(inputImage, 180);
-    writeImage(blackWhite, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/black-white.pbm");
-    
-       // create noiseImage:
-
-
-    // Sequence of expand shrink shrink expand image
-    
-    printf("0. expand image\n\tbc white is 8 neighbor\n");
-    Image imageCleaned = imageExpand(blackWhite);
-    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
-    
-    printf("1. shrink image\n\tremoves most black noise\n");
-    imageCleaned = imageShrink(imageCleaned);
-    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
-    
-    printf("2. shrink image\n\teliminates rest of black noise\n");
-    imageCleaned = imageShrink(imageCleaned);
-    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
-
-    printf("4. expand image\n\tthicken lines\n");
-    imageCleaned = imageExpand(imageCleaned);
-    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
+//    printf("creating black white image\n");
+//    Image blackWhite = imageBlackWhite(inputImage, 180);
+//    writeImage(blackWhite, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/black-white.pbm");
+//    
+//       // create noiseImage:
+//    Image noiseImage = imageNoise(cleanImage, 0.6);
+//    writeImage(noiseImage, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/noise.pbm");
+//
+//    // Sequence of expand shrink shrink expand image
+//    printf("0. expand image\n\tbc white is 8 neighbor\n");
+//    Image imageCleaned = imageExpand(blackWhite);
+//    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
+////    
+//    printf("1. shrink image\n\tremoves most black noise\n");
+//    imageCleaned = imageShrink(imageCleaned);
+//    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
+////    
+//    printf("2. shrink image\n\teliminates rest of black noise\n");
+//    imageCleaned = imageShrink(imageCleaned);
+//    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
+//
+//    printf("4. expand image\n\tthicken lines\n");
+//    imageCleaned = imageExpand(imageCleaned);
+//    writeImage(imageCleaned, "/Users/ninawang/Documents/School/CS136/testProj/netpbm/image_cleaned.pbm");
 
        //-------------------------------------------------------------------------------
        // Uncomment this after you finish your homework
@@ -87,10 +71,9 @@ int main(int argc, const char * argv[]) {
 
         /* Delete back and white, noise, Expand and Shrink */
     deleteImage(inputImage);
-//    deleteImage(blackWhite);
+    deleteMatrix(components);
+//    deleteImage(textColoredImage);
     deleteImage(cleanImage);
-//       deleteImage(rotatedImage);
-//       deleteImage(invertedImage);
 
 
 
@@ -194,10 +177,13 @@ Image imageNoise(Image orig, float p) {
 
 // only for binary images
 Matrix componentLabeling(Image img) {
+    // TRY CREATEMATRIXFROMARRAY TO FILL WITH -1
+//    double arr[img.width*img.height];
+//    memset(arr, -1.0, sizeof(double));
+//    Matrix comp = createMatrixFromArray(arr, img.height, img.width);
     Matrix comp = createMatrix(img.height, img.width); // matrix to look at for labeling
 //    int eqTable[(img.height*img.width)/2 + 1];
     int eqTable[(img.height*img.width)];
-    int eqTableIndex = 0;
     int label = 0;
     
     // initialize away garbage values in my equivalence table
@@ -206,11 +192,11 @@ Matrix componentLabeling(Image img) {
     }
     
     // columns
-    for (int i = 0; i < img.height - 1; i++) {
+    for (int i = 0; i < img.height; i++) {
         // rows
-        for (int j = 0; j < img.width - 1; j++) {
-            // if the pixel is black
-            if (img.map[i][j].i == 1) {
+        for (int j = 0; j < img.width; j++) {
+            // if the pixel is black 0 (white is 255)
+            if (img.map[i][j].i == 0) {
                 if (i == 0 && j == 0) {
                     // if pixel is top left
                     eqTable[label*2] = label;
@@ -220,7 +206,7 @@ Matrix componentLabeling(Image img) {
                     label++;
                 } else if (j == 0) {
                     // leftmost pixels
-                    if (img.map[i-1][j].i == 1) {
+                    if (img.map[i-1][j].i == 0) {
                         // if upper pixel is labeled
                         comp.map[i][j] = comp.map[i-1][j];
                     } else {
@@ -230,18 +216,29 @@ Matrix componentLabeling(Image img) {
                         comp.map[i][j] = label;
                         label++;
                     }
-                } else if (img.map[i-1][j].i == 1 && img.map[i][j-1].i == 1) {
+                } else if (i == 0) {
+                    // topmost pixels
+                    if (img.map[i][j-1].i == 0) {
+                        comp.map[i][j] = comp.map[i][j-1];
+                    } else {
+                        eqTable[label*2] = label;
+                        eqTable[label*2+1] = label;
+                        
+                        comp.map[i][j] = label;
+                        label++;
+                    }
+                } else if (img.map[i-1][j].i == 0 && img.map[i][j-1].i == 0) {
                     // if top and left neighbor are black
                     comp.map[i][j] = comp.map[i-1][j]; // label is top label regardless
                     
                     if (comp.map[i-1][j] != comp.map[i-1][j]) {
                         // pairs of indexes in the array are equivalent
-                        eqTable[(int) comp.map[i][j-1]*2+1] = comp.map[i-1][j];
+                        eqTable[(int) comp.map[i][j-1]*2+1] = comp.map[i-1][j]; // setting left group to top group
                     }
-                } else if (img.map[i-1][j].i == 1) {
+                } else if (img.map[i-1][j].i == 0) {
                     // only top has label
                     comp.map[i][j] = comp.map[i-1][j];
-                } else if (img.map[i][j-1].i == 1) {
+                } else if (img.map[i][j-1].i == 0) {
                     // only left has label
                     comp.map[i][j] = comp.map[i][j-1];
                 } else {
@@ -258,20 +255,88 @@ Matrix componentLabeling(Image img) {
     }
     
     // find lowest label
-    for (int i = 0; i < (img.height*img.width); i++) {
-        if (eqTable[i*2] != eqTable[i*2+1]) {
-            eqTable[i*2+1] = eqTable[eqTable[i*2]+1];
+    int x = 0;
+    // x*2+1 WAS x PREVIOUSLY
+    while (eqTable[x*2+1] != -1) { // might not be good? but also for this use case, it  won't  fill up
+        if (eqTable[x*2] != eqTable[x*2+1]) {
+//            eqTable[x*2+1] = eqTable[(int)eqTable[x*2]+1];
+            eqTable[x*2+1] = eqTable[(int)eqTable[x*2+1]];
         }
+        x++;
     }
     
-    // second pass through picture
-    for (int i = 0; i < img.height - 1; i++) {
-        for (int j = 0; j < img.width - 1; j++) {
-            if (img.map[i][j].i == 1) {
+//     second pass through picture
+    for (int i = 0; i < img.height; i++) {
+        for (int j = 0; j < img.width; j++) {
+            if (img.map[i][j].i == 0) {
                 comp.map[i][j] = eqTable[((int)comp.map[i][j])*2+1];
             }
         }
     }
     
     return comp;
+}
+
+Image componentColoring(Image orig, Matrix comp, int thresh) {
+    
+    for (int i = 0; i < orig.height; i++) {
+        for (int j = 0; j < orig.width; j++) {
+            if((int) comp.map[i][j] != 0) {
+                setPixel(orig, i, j, 245, (int)comp.map[i][j]*0.255, 66, NO_CHANGE);
+            }
+            
+//            switch ((int) comp.map[i][j]) {
+//                case 1:
+//                    setPixel(orig, i, j, 245, 30, 66, NO_CHANGE);
+//                    break;
+//                case 65:
+//                    setPixel(orig, i, j, 245, 66, 66, NO_CHANGE);
+//                    break;
+//                case 66:
+//                    setPixel(orig, i, j, 245, 130, 66, NO_CHANGE);
+//                    break;
+//                case 67:
+//                    setPixel(orig, i, j, 245, 200, 66, NO_CHANGE);
+//                    break;
+//            }
+        }
+    }
+    return orig;
+    
+    
+//    int components[orig.height*orig.width/2+1];
+//    
+//    for (int i = 0; i < (orig.height*orig.width/2+1); i++) {
+//        components[i] = -1;
+//    }
+//    
+//    for (int i = 0; i < orig.height; i++) {
+//        for (int j = 0; j < orig.width; j++) {
+//            // count the amount of pixels per component for the threshold
+////            components[(int)comp.map[i][j]*2+1] = components[(int)comp.map[i][j]];
+//            components[(int)comp.map[i][j]] += 1;
+//            // comp.map[i][j] is the label, which is being used as the index
+//        }
+//    }
+//    
+//    int r = 245;
+//    int g = 66;
+//    int b = 66;
+//    for (int i = 0; i < orig.height; i++) {
+//        for (int j = 0; j < orig.width; j++) {
+//            if((int)comp.map[i][j] == 68) {
+//                setPixel(orig, i, j, r, g, b, NO_CHANGE);
+//            }
+////            if (orig.map[i][j].i == 0) {
+////                if (components[(int)comp.map[i][j]] >= 50) {
+////                    setPixel(orig, i, j, r, g, b, NO_CHANGE);
+////                }
+////                if((int)comp.map[i][j] == 70) {
+////                    setPixel(orig, i, j, r, g, b, NO_CHANGE);
+////                }
+//                
+////            }
+//        }
+//    }
+//    return orig;
 }
